31c924baf37beac57748e31f3d464d8c5be1a0dd
nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2024-01-23 22:08:22 +0200

    add entering inventory into inventory interactor

--- resources/inventory/scripts/inventory_interactor.gd
+++ resources/inventory/scripts/inventory_interactor.gd
@@ -3,6 +3,8 @@ class_name InventoryInteractor
 signal inventory_container_area_entered(inventory:InventoryContainer)
 signal inventory_container_area_exited
 
+var entering_inventory:InventoryContainer = null
+
 func _ready():
 	monitorable = false
 	area_entered.connect(_on_area_entered)
@@ -11,7 +13,9 @@ func _ready():
 func _on_area_entered(area):
 	if area is InventoryContainer:
 		inventory_container_area_entered.emit(area)
+		entering_inventory = area
 
 func _on_area_exited(area):
 	if area is InventoryContainer:
 		inventory_container_area_exited.emit()
+		entering_inventory = null
