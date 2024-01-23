f960920625f4ac53591f857f6ae078dbc76aa603
nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2024-01-23 21:33:15 +0200

    changed components names and add signals to inventory container

--- resources/prefabs/player/player.gd
+++ resources/prefabs/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal inventory_container_area_entered(inventory:Inventory)
-signal inventory_container_area_exited
+signal inventory_opened(inventory:Inventory)
+signal inventory_closed
 
 ##TODO change this to normal movement
 const SPEED = 300.0
@@ -14,7 +14,7 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func _get_direction() -> Vector2:
-	var direction:Vector2
+	var direction:Vector2 = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
 		direction.y = -1
 	elif Input.is_action_pressed("ui_down"):
@@ -31,9 +31,9 @@ func _on_hot_bar_current_slot_changed(_current_slot_index):
 	if item_marker.get_child_count() != 0:
 		item_marker.get_child(0).queue_free()
 
-
-func _on_inventory_interactor_area_inventory_container_area_entered(inventory):
-	inventory_container_area_entered.emit(inventory)
-	
-func _on_inventory_interactor_area_inventory_container_area_exited():
-	inventory_container_area_exited.emit()
+##TODO make it in state interact_inventory
+func _on_inventory_container_area_entered(inventory:InventoryContainer):
+	inventory_opened.emit(inventory.inventory)
+##TODO make it in state interact_inventory	
+func _on_inventory_container_area_exited():
+	inventory_closed.emit()
