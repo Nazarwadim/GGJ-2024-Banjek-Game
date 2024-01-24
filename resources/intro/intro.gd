56cb1f1b42d22f67f1dc4fbfb8e6c5574e5712da
BogdanVereshchak <bohdan.vereshchak.pp.2022@lpnu.ua>
2024-01-24 16:17:52 +0200

    Added second variation of intro
    I added variation for intro in 3d space with more details and smoother

---
+++ resources/intro/intro.gd
@@ -0,0 +1,11 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_animation_finished(anim_name):
+	#TODO change "menu.tscn" to actual existing menu scene
+	#get_tree().change_scene_to_file("res://recources/menu/Menu.tscn")
+	pass
