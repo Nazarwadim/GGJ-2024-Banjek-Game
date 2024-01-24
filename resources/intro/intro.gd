ac7260187d736f008449708c767fb9c78b70c3f6
BogdanVereshchak <bohdan.vereshchak.pp.2022@lpnu.ua>
2024-01-24 17:21:44 +0200

    Keep best intro scene
    Deleted bad intro and only keep good one

--- resources/intro/intro.gd
+++ resources/intro/intro.gd
@@ -5,6 +5,7 @@ extends AnimationPlayer
 func _ready():
 	pass # Replace with function body.
 
+
 func _on_animation_finished(anim_name):
 	#TODO change "menu.tscn" to actual existing menu scene
 	#get_tree().change_scene_to_file("res://recources/menu/Menu.tscn")
