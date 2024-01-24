70652e8867bcb2f8074c8b5abcc78e556830bb3a
nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2024-01-24 17:03:19 +0200

    state mashine

--- resources/prefabs/player/states/idle.gd
+++ resources/prefabs/player/states/idle.gd
@@ -1,25 +1,41 @@
 extends PlayerBaseState
 
+@export var _inventory_interactor:InventoryInteractor
+@export var _item_interactor:ItemInteractor
+
+func _ready():
+	if _inventory_interactor == null:
+		push_error("Set inventory interactor for state Walk")
+	if _item_interactor == null:
+		push_error("Set item interactor for state Walk")
+
 func on_enter() -> void:
 	await get_tree().create_timer(get_process_delta_time()).timeout
 	player.animation_tree.get("parameters/playback").travel("Idle")
 
-func on_process(_delta: float) -> void:
+func on_physics_process(_delta: float) -> void:
+	if(_try_switch_item_pick_up()):
+		return
+	if(_try_switch_inventory_interaction()):
+		return
+	_try_switch_walk()
+	
+
+func _try_switch_walk() -> bool:
 	var player_direction :Vector2 = player.get_input_vector_normalized()
 	if player_direction != Vector2.ZERO:
 		change_state("Walk")
+		return true
+	return false
+
+func _try_switch_item_pick_up() -> bool:
+	if _item_interactor.entering_item != null && Input.is_action_pressed("interact"):
+		change_state("InventoryInteraction")
+		return true
+	return false
 	
-	
-
-# Called every physics frame when this state is active.
-func on_physics_process(_delta: float) -> void:
-	pass
-
-
-# Called when there is an input event while this state is active.
-func on_input(_event: InputEvent) -> void:
-	pass
-
-# Called when the state machine exits this state.
-func on_exit() -> void:
-	pass
+func _try_switch_inventory_interaction() -> bool:
+	if _item_interactor.entering_item != null && Input.is_action_pressed("interact"):
+		change_state("PickingUpItem")
+		return true
+	return false
--- resources/prefabs/player/states/walk.gd
+++ resources/prefabs/player/states/walk.gd
@@ -1,15 +1,47 @@
 extends PlayerBaseState
-var player_direction:Vector2
+
+@export var _inventory_interactor:InventoryInteractor
+@export var _item_interactor:ItemInteractor
+
+var _player_direction:Vector2
+
+func _ready():
+	if _inventory_interactor == null:
+		push_error("Set inventory interactor for state Walk")
+	if _item_interactor == null:
+		push_error("Set item interactor for state Walk")
 
 func on_enter() -> void:
 	player.animation_tree.get("parameters/playback").travel("Walk")
 	
-	
 func on_physics_process(_delta: float) -> void:
-	player_direction = player.get_input_vector_normalized()
-	player.velocity = player_direction * player.speed
-	if player_direction == Vector2.ZERO:
+	if _try_switch_item_pick_up():
+		return
+	if _try_switch_inventory_interaction():
+		return
+	_player_direction = player.get_input_vector_normalized()
+	if not _try_switch_idle():
+		_set_animation_tree_blend_position()
+
+func _set_animation_tree_blend_position() -> void:
+	player.animation_tree.set("parameters/Idle/blend_position", _player_direction)
+	player.animation_tree.set("parameters/Walk/blend_position", _player_direction)
+
+func _try_switch_idle() -> bool:
+	player.velocity = _player_direction * player.speed
+	if _player_direction == Vector2.ZERO:
 		change_state("Idle")
-	else:
-		player.animation_tree.set("parameters/Idle/blend_position",player_direction)
-		player.animation_tree.set("parameters/Walk/blend_position",player_direction)
+		return true
+	return false
+
+func _try_switch_item_pick_up() -> bool:
+	if _item_interactor.entering_item != null && Input.is_action_pressed("interact"):
+		change_state("InventoryInteraction")
+		return true
+	return false
+	
+func _try_switch_inventory_interaction() -> bool:
+	if _item_interactor.entering_item != null && Input.is_action_pressed("interact"):
+		change_state("PickingUpItem")
+		return true
+	return false
