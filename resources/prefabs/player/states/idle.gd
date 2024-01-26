6d0bdd4a490eb04dc25370badd834ad273fba0fd
nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2024-01-26 11:03:53 +0200

    remove on phisics processs input from state idle

--- resources/prefabs/player/states/idle.gd
+++ resources/prefabs/player/states/idle.gd
@@ -19,9 +19,6 @@ func on_enter() -> void:
 	player.velocity = Vector2.ZERO
 	player.animation_tree.get("parameters/playback").travel("Idle")
 
-func on_physics_process(_delta: float) -> void:	
-	_try_switch_walk()
-
 func on_input(input:InputEvent):
 	if _interaction_state_checker.can_switch_item_pick_up():
 		change_state("PickingUpItem")
@@ -30,22 +27,27 @@ func on_input(input:InputEvent):
 	elif _interaction_state_checker.can_switch_droping_item():
 		change_state("DropingItem")
 	elif input.is_action_pressed("interact"):
-		var area_using_item := area_using_item_detector.get_first_overlapping_area_using_item()
-		if area_using_item == null:
-			return
-		var hot_bar_slot_index := player.get_hot_bar_slot_index()
-		if inventory_container.inventory.items[hot_bar_slot_index] == null:
-			return
-		var current_hot_bar_item_name := inventory_container.inventory.items[hot_bar_slot_index].name
-		if area_using_item.item_can_use.name != current_hot_bar_item_name:
-			return
-		if area_using_item.is_used:
-			return
-		change_state("UsingItem")
+		if _can_switch_using_item_if_preased_interact():
+			change_state("UsingItem")
+	elif _can_switch_walk():
+		change_state("Walk")
 	
-func _try_switch_walk() -> bool:
+func _can_switch_using_item_if_preased_interact() -> bool:
+	var area_using_item := area_using_item_detector.get_first_overlapping_area_using_item()
+	if area_using_item == null:
+		return false
+	var hot_bar_slot_index := player.get_hot_bar_slot_index()
+	if inventory_container.inventory.items[hot_bar_slot_index] == null:
+		return false
+	var current_hot_bar_item_name := inventory_container.inventory.items[hot_bar_slot_index].name
+	if area_using_item.item_can_use.name != current_hot_bar_item_name:
+		return false
+	if area_using_item.is_used:
+		return false
+	return true
+	
+func _can_switch_walk() -> bool:
 	var player_direction :Vector2 = player.get_input_vector_normalized()
 	if player_direction != Vector2.ZERO:
-		change_state("Walk")
 		return true
 	return false
