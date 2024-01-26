846714c93727a5789063af9490f947da25931a17
nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2024-01-26 15:53:17 +0200

    refactoring this big sh.. player mood for normal mouse observer

--- resources/gui/mouse_entered_object_observer/mouse_entered_object_observer.gd
+++ resources/gui/mouse_entered_object_observer/mouse_entered_object_observer.gd
@@ -1,33 +1,21 @@
 extends Control
 
-@export var _hot_bar:Control
+signal slot_mouse_entered(slot)
+signal slot_mouse_exited(slot)
 
-@export var _player_mood_sprite : Sprite2D
-@onready var _hot_bar_inventory :Inventory = _hot_bar.inventory
-# Mood priorities:
-# Can`t use -> highest priority = 2
-# Can use = 1
-# Tip = 3
-# standart nothing = 0
+signal item_container_mouse_entered(item_container)
+signal item_container_mouse_exited(item_container)
 
-enum State
-{
-	Standart,
-	CanUse,
-	CantUse,
-	Tip
-}
+signal inventory_container_mouse_entered(inventory_container)
+signal inventory_container_mouse_exited(inventory_container)
 
-
+signal area_using_item_mouse_entered(area_using_item)
+signal area_using_item_mouse_exited(area_using_item)
 
 var _area_using_item_containers:Array[Node] = []
 var _chest_containers:Array[Node] = []
 var _items:Array[Node] = []
 var _slots:Array[Node] = []
-
-var _blocked := false
-var _current_state := State.Standart
-var _current_area_using_item:AreaUsingItem
 
 func _ready():
 	await get_tree().create_timer(get_physics_process_delta_time() + 0.1).timeout
@@ -37,130 +25,55 @@ func _ready():
 	_slots = get_tree().get_nodes_in_group("slots")
 	
 	for slot in _slots:
-		slot.mouse_entered.connect(_on_item_ui_mouse_entered.bind(slot.item))
-		slot.mouse_exited.connect(_on_item_ui_mouse_exited)
+		slot.mouse_entered.connect(_on_item_ui_mouse_entered.bind(slot))
+		slot.mouse_exited.connect(_on_item_ui_mouse_exited.bind(slot))
 	
 	for item in _items:
-		item.mouse_entered.connect(_on_item_container_mouse_entered)
-		item.mouse_exited.connect(_on_item_container_mouse_exited)
+		item.mouse_entered.connect(_on_item_container_mouse_entered.bind(item))
+		item.mouse_exited.connect(_on_item_container_mouse_exited.bind(item))
 	
 	for chest in _chest_containers:
-		chest.mouse_entered.connect(_on_item_container_mouse_entered)
-		chest.mouse_exited.connect(_on_item_container_mouse_exited)
+		chest.mouse_entered.connect(_on_item_container_mouse_entered.bind(chest))
+		chest.mouse_exited.connect(_on_item_container_mouse_exited.bind(chest))
 	
 	for area_using_item in _area_using_item_containers:
 		area_using_item.mouse_entered.connect(_on_area_using_item_mouse_entered.bind(area_using_item))
-		area_using_item.mouse_exited.connect(_on_area_using_item_mouse_exited)
+		area_using_item.mouse_exited.connect(_on_area_using_item_mouse_exited.bind(area_using_item))
 	
 	SignalBus.item_container_tree_entered.connect(_on_item_droped)
 	SignalBus.slot_ui_tree_entered.connect(_on_slot_entered_tree)
-	_hot_bar.current_slot_changed.connect(_on_hot_bar_slot_changed)
-	_hot_bar.hot_bar_updated.connect(_on_hot_bar_updated)
 
 
-
-#region PlayerMood
 func _on_slot_entered_tree(slot:Control):
-	slot.mouse_entered.connect(_on_item_ui_mouse_entered.bind(slot.item))
-	slot.mouse_exited.connect(_on_item_ui_mouse_exited)
+	slot.mouse_entered.connect(_on_item_ui_mouse_entered.bind(slot))
+	slot.mouse_exited.connect(_on_item_ui_mouse_exited.bind(slot))
 
 func _on_item_droped(item_container:CollisionObject2D):
-	item_container.mouse_entered.connect(_on_item_container_mouse_entered)
-	item_container.mouse_exited.connect(_on_item_container_mouse_exited)
+	item_container.mouse_entered.connect(_on_item_container_mouse_entered.bind(item_container))
+	item_container.mouse_exited.connect(_on_item_container_mouse_exited.bind(item_container))
 
-func _on_item_ui_mouse_entered(item:Item) -> void:
-	if item == null:
-		return
-	_current_state = State.Tip
-	if not _blocked:
-		_player_mood_sprite.frame = State.Tip
+func _on_item_ui_mouse_entered(item) -> void:
+	slot_mouse_entered.emit(item)
 
-func _on_item_ui_mouse_exited() -> void:
-	_current_state = State.Standart
-	if not _blocked:
-		_player_mood_sprite.frame = State.Standart
+func _on_item_ui_mouse_exited(item) -> void:
+	slot_mouse_exited.emit(item)
 
 	
-func _on_item_container_mouse_entered() -> void:
-	_current_state = State.Tip
-	if not _blocked:
-		_player_mood_sprite.frame = State.Tip
+func _on_item_container_mouse_entered(item_container) -> void:
+	item_container_mouse_entered.emit(item_container)
 
-func _on_item_container_mouse_exited() -> void:
-	_current_state = State.Standart
-	if not _blocked:
-		_player_mood_sprite.frame = State.Standart
+func _on_item_container_mouse_exited(item_container) -> void:
+	item_container_mouse_exited.emit(item_container)
 
 
-func _on_inventory_container_mouse_entered() -> void:
-	_current_state = State.Tip
-	if not _blocked:
-		_player_mood_sprite.frame = State.Tip
+func _on_inventory_container_mouse_entered(inventory_container) -> void:
+	inventory_container_mouse_entered.emit(inventory_container)
 	
-func _on_inventory_container_mouse_exited() -> void:
-	_current_state = State.Standart
-	if not _blocked:
-		_player_mood_sprite.frame = State.Standart
+func _on_inventory_container_mouse_exited(inventory_container) -> void:
+	inventory_container_mouse_exited.emit(inventory_container)
 
-func _on_hot_bar_updated():
-	_on_hot_bar_slot_changed(_hot_bar.get_current_slot_index())
-
-func _on_hot_bar_slot_changed(slot_index:int):
-	if _current_area_using_item != null:
-		if _current_area_using_item.is_used:
-			_current_state = State.Standart
-		elif  _hot_bar_inventory.items[slot_index] != null &&\
-		  _hot_bar_inventory.items[slot_index].name == _current_area_using_item.item_can_use.name:
-			_current_state = State.CanUse
-		elif _hot_bar_inventory.items[slot_index] != null:
-			_current_state = State.CantUse
-		else:
-			_current_state = State.Tip
-		if not _blocked:
-			_player_mood_sprite.frame = _current_state
-
-func _on_area_using_item_mouse_entered(item:AreaUsingItem) -> void:
-	_current_area_using_item = item
-	var hot_bar_index :int = _hot_bar.get_current_slot_index()
-	if item.is_used:
-		_current_state = State.Standart
-	elif  _hot_bar_inventory.items[ hot_bar_index] != null &&\
-		_hot_bar_inventory.items[hot_bar_index].name == _current_area_using_item.item_can_use.name:
-		_current_state = State.CanUse
-	elif _hot_bar_inventory.items[ hot_bar_index] != null:
-		_current_state = State.CantUse
-	else:
-		_current_state = State.Tip
-	if not _blocked:
-		_player_mood_sprite.frame = _current_state
+func _on_area_using_item_mouse_entered(area_using_item) -> void:
+	area_using_item_mouse_entered.emit(area_using_item)
 	
-func _on_area_using_item_mouse_exited() -> void:
-	_current_area_using_item = null
-	_current_state = State.Standart
-	if not _blocked:
-		_player_mood_sprite.frame = _current_state
-
-func set_player_cant_use_for_time(time_ms:int) -> void:
-	if _blocked:
-		return
-	var temp_state := _current_state
-	_current_state = State.CantUse
-	_blocked = true
-	_player_mood_sprite.frame = _current_state
-	await get_tree().create_timer(time_ms * 1000).timeout
-	_blocked = false
-	_current_state = temp_state
-	_player_mood_sprite.frame = _current_state
-
-func set_player_can_use_for_time(time_ms:int) -> void:
-	if _blocked:
-		return
-	var temp_state := _current_state
-	_current_state = State.CanUse
-	_blocked = true
-	_player_mood_sprite.frame = _current_state
-	await get_tree().create_timer(time_ms * 1000).timeout
-	_blocked = false
-	_current_state = temp_state
-	_player_mood_sprite.frame = _current_state
-#endregion
+func _on_area_using_item_mouse_exited(area_using_item) -> void:
+	area_using_item_mouse_exited.emit(area_using_item)
