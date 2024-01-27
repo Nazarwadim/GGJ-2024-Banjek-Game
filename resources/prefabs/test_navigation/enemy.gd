65a5cb7d2191cd00d8793cffc99fd5323d56c500
nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2024-01-27 11:50:23 +0200

    add enemy avoidance

--- resources/prefabs/test_navigation/enemy.gd
+++ resources/prefabs/test_navigation/enemy.gd
@@ -6,15 +6,18 @@ extends CharacterBody2D
 @onready var navigation_agent := $NavigationAgent2D as NavigationAgent2D
 
 func _physics_process(delta):
-	move_and_slide()
+	move_and_slide.call_deferred()
 	if navigation_agent.is_navigation_finished():
-		return
-	
+		return	
 	var direction = to_local(navigation_agent.get_next_path_position()).normalized()
-	velocity = direction * MAX_SPEED
+	var intermediate_velosity :Vector2= direction * MAX_SPEED
+	navigation_agent.velocity = intermediate_velosity
 
 func create_path():
 	navigation_agent.target_position = target.global_position
 
 func _on_timer_timeout():
 	create_path()
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity):
+	velocity = safe_velocity
