6d82d380f8e8f8bcebc1b339d099dc21cd1d6976
Nazarwadim <116561933+Nazarwadim@users.noreply.github.com>
2024-01-28 02:26:06 +0200

    Introduction (#44)
    * refactor
    
    * Create simple introduction scene with timer
    
    Add introduction with bad text(to be changed)
    
    * add ^press enter to skip^
    
    * godot
    
    * start timers when scene falling
    
    ---------
    
    Co-authored-by: BogdanVereshchak <bohdan.vereshchak.pp.2022@lpnu.ua>

---
+++ resources/menu/introduction_to_game.gd
@@ -0,0 +1,22 @@
+extends Control
+@export var start_scene : PackedScene 
+
+@export var cant_skip_duration : int = 10
+@export var introduction_duration : int = 30
+
+var _can_skip:bool = false
+			
+func _on_scene_falling():
+	get_tree().create_timer(introduction_duration).timeout.connect(_on_timer_time_out)
+	get_tree().create_timer(cant_skip_duration).timeout.connect(_on_cant_skip_timeout)
+
+func _on_cant_skip_timeout() -> void:
+	$CanSkipLabel/LabelAnimationPlayer.play("start_showing")
+	_can_skip = true
+
+func _on_timer_time_out() -> void:
+	get_tree().change_scene_to_packed.call_deferred(start_scene)
+
+func _input(event):
+	if event.is_action_pressed("skip") and _can_skip:
+		get_tree().change_scene_to_packed.call_deferred(start_scene)
