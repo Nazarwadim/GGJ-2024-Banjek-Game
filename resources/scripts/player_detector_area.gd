51eae943ac96dce9496e1b07181d2e361c2b3e4f
Nazarwadim <116561933+Nazarwadim@users.noreply.github.com>
2024-01-28 14:48:50 +0200

    Add arrow (#51)
    * fix player chase
    
    * add win and die
    
    * add arrow

--- resources/scripts/player_detector_area.gd
+++ resources/scripts/player_detector_area.gd
@@ -4,6 +4,15 @@ class_name PlayerDetectorArea
 signal player_entered(player)
 signal player_exited(player)
 
+func get_first_overlapping_player() -> Player:
+	var areas := get_overlapping_bodies()
+	if areas.size() == 0:
+		return null
+	for area in areas:
+		if area is Player:
+			return area
+	return null
+
 func _ready():
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
